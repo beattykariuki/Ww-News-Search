e02b59e6717af7cdc9af83eab5ef5f0cd6b7947a
beattykariuki <bettykariuki026@gmail.com>
2018-11-14 14:51:34 +0300

    from app imported app

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
 
+    popular_news = get_news('popular')
+    print(popular_news)
     title = 'Home - Welcome To News Search'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_news)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
