bafdac9a48857075efdd83e0c6fa2c3803c1a1b6
beattykariuki <bettykariuki026@gmail.com>
2018-11-14 14:38:33 +0300

    added headlines_base_url and sources_base_url

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 class Config:
 
+    HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?category={}country=us&apiKey={}'
+    SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
     pass
 
 class ProdConfig(Config):
